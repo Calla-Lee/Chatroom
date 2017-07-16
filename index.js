9706de51ef7aaa0933a1d5ceb397bef9f2470ffa
Payton Bice <hellopayton@gmail.com>
2017-07-16 13:02:35 -0500

    Changes UID assignment and spacing for function declarations

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var userID = 0;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  console.log('User #' + userID + ' has connected.');
-  if(userID = 0){
-    userID = 0;
-  }else {
-    userID++;
-  }
+io.on('connection', function (socket) {
+  ++userID;
 
-  socket.on('chat message', function(msg){
+  console.log('User #' + userID + ' has connected.');
+
+  socket.on('chat message', function (msg) {
     console.log('User #' + userID + ': ' + msg);
   });
 
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function (msg) {
     io.emit('User #' + userID + ': ', msg);
   });
-
 });
 
-http.listen(1738, function(){
+http.listen(1738, function () {
   console.log('listening on *:1738');
 });
