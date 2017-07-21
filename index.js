5beeb7b647f458330bcec84422ad0cf3010af7e1
Payton Bice <hellopayton@gmail.com>
2017-07-20 20:29:05 -0500

    Adding notes in the comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// I've declared these as constant since we don't expect the values to change
 const app   = require('express')();
 const http  = require('http').Server(app);
 const io    = require('socket.io')(http);
@@ -9,8 +10,10 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   // Add the user to the channel
+  // (Read about the difference between `let` vs. `var` if you're curious)
   let user = users.add(socket);
 
+  // (And about string interpolation if this is confusing syntax to you.)
   console.log(`User #${user.id} has connected`);
 
   socket.on('chat message', function (msg) {
